5f17bb4ea2dae8102de56f22191257e06111aa5d
Christophe de Batz <christophe.db@gmail.com>
2016-03-04 01:08:27 +0100

    feat(groups): add several controller method (get all groups, get group by id and save new group)

--- mistergift-api/src/main/java/com/gvstave/mistergift/admin/controller/GroupController.java
+++ mistergift-api/src/main/java/com/gvstave/mistergift/admin/controller/GroupController.java
@@ -1,15 +1,19 @@
 package com.gvstave.mistergift.admin.controller;
 
+import com.gvstave.mistergift.admin.controller.exception.InvalidFieldValueException;
+import com.gvstave.mistergift.admin.controller.exception.UnauthorizedOperationException;
 import com.gvstave.mistergift.admin.response.PageResponse;
 import com.gvstave.mistergift.config.annotation.UserRestricted;
 import com.gvstave.mistergift.data.domain.Group;
 import com.gvstave.mistergift.data.persistence.GroupPersistenceService;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 
+@UserRestricted
 @RestController
 @RequestMapping(value = "/groups", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GroupController extends AbstractController {
@@ -23,12 +27,45 @@ public class GroupController extends AbstractController {
      *
      * @return Serialized groups list.
      */
-    @UserRestricted
+
     @RequestMapping(method = RequestMethod.GET)
     public @ResponseBody PageResponse<Group> getGroups(
             @RequestParam(value = "page", required = false, defaultValue = "1") Integer page) {
         PageRequest pageRequest = getPageRequest(page);
         return new PageResponse<>(groupPersistenceService.findAll(pageRequest));
+    }
+
+    /**
+     * Returns a group by its id.
+     *
+     * @param id The group id.
+     * @return a serialized {@link Group}.
+     */
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public @ResponseBody Group getGroupById(@PathVariable(value = "id") Long id) {
+        return groupPersistenceService.findOne(id);
+    }
+
+    /**
+     * Save new group in database.
+     *
+     * @param group The group.
+     * @throws UnauthorizedOperationException
+     * @throws InvalidFieldValueException
+     */
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE })
+    public @ResponseBody Group save(@RequestBody Group group) throws UnauthorizedOperationException, InvalidFieldValueException {
+        ensureGroupValid(group, false);
+        return groupPersistenceService.save(group);
+    }
+
+    /**
+     *
+     * @param group
+     * @param isUpdate
+     */
+    private void ensureGroupValid(Group group, boolean isUpdate) {
     }
 
 }
