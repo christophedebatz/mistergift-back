229bea61a5e0a19ed28739078b149f0e4fb3ac63
Christophe de Batz <christophe.db@gmail.com>
2016-11-03 00:39:44 +0100

    refactoring(geoloc): refactoring of whole code + deltas

--- mistergift-api/src/main/java/com/gvstave/mistergift/service/geoip/GeolocationService.java
+++ mistergift-api/src/main/java/com/gvstave/mistergift/service/geoip/GeolocationService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -18,16 +17,16 @@ import java.util.Objects;
 import java.util.Optional;
 
 /**
- * It allow client geolocalizing.
+ * It allows client geolocalization.
  */
 @Service
 public class GeolocationService {
 
-    /** The logger. */
-    private static Logger LOGGER = LoggerFactory.getLogger(GeolocationService.class);
-
     /** The geolocation externa API endpoint service. */
     private static final String SERVICE_SERVER_URL = "http://api.ipinfodb.com/v3/ip-city/?key=%s&ip=%s&format=json";
+
+    /** The logger. */
+    private static Logger LOGGER = LoggerFactory.getLogger(GeolocationService.class);
 
     /** The environment. */
     @Inject
@@ -39,11 +38,11 @@ public class GeolocationService {
      * @param request The servlet request.
      * @return The client IP.
      */
-    public String requestClientIp(HttpServletRequest request) {
-        System.setProperty("java.net.preferIPv4Stack" , "true");
+    public String requestClientIp (HttpServletRequest request) {
+        System.setProperty("java.net.preferIPv4Stack", "true");
         String ipAddress = request.getHeader("X-FORWARDED-FOR");
 
-        // try getting header from nging proxy pass
+        // try getting header from nginx proxy pass
         if (ipAddress == null) {
             ipAddress = request.getHeader("X-Real-IP");
         }
@@ -60,39 +59,39 @@ public class GeolocationService {
      *
      * @param ip The client ip.
      * @return The city.
-     * @throws IOException If output stream is corrupted.
      */
-    public Optional<GeolocationResult> requestClientGeolocation(String ip) throws IOException {
+    public Optional<GeolocationResult> requestClientGeolocation (String ip) {
         Objects.requireNonNull(ip);
         LOGGER.debug("Ip Address is " + ip);
 
-        URL url = new URL(String.format(SERVICE_SERVER_URL, environment.getProperty("service.ipinfodb.key"), ip));
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
+        try {
+            URL url = new URL(String.format(SERVICE_SERVER_URL, environment.getProperty("service.ipinfodb.key"), ip));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
 
-        if (connection.getResponseCode() == 200) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            ObjectMapper objectMapper = new ObjectMapper();
-            StringBuilder response = new StringBuilder();
-            String inputLine;
+            if (connection.getResponseCode() == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                ObjectMapper objectMapper = new ObjectMapper();
+                StringBuilder response = new StringBuilder();
+                String inputLine;
 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                byte[] jsonData = response.toString().getBytes();
+                JsonNode rootNode = objectMapper.readTree(jsonData);
+
+                if (rootNode.size() > 0) {
+                    String countryName = rootNode.path("countryName").asText();
+                    String regionName = rootNode.path("regionName").asText();
+                    String cityName = rootNode.path("cityName").asText();
+                    return Optional.of(new GeolocationResult(countryName, regionName, cityName));
+                }
             }
-            in.close();
-
-            byte[] jsonData = response.toString().getBytes();
-            JsonNode rootNode = objectMapper.readTree(jsonData);
-
-            if (rootNode.size() > 0) {
-                return Optional.of(
-                    new GeolocationResult(
-                        rootNode.path("countryName").asText(),
-                        rootNode.path("regionName").asText(),
-                        rootNode.path("cityName").asText()
-                    )
-                );
-            }
+        } catch (Exception e) {
+            LOGGER.error("Unable to get user IP.", e);
         }
 
         return Optional.empty();
