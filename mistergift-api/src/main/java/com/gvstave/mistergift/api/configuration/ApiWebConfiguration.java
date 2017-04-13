374fc89dd7ea4961adb6ae1c62a7d839b56934f3
Christophe de Batz <christophe.db@gmail.com>
2017-04-13 18:26:30 +0200

    feat(jackson): add mapper to jackson to unserialize date in iso 8601

--- mistergift-api/src/main/java/com/gvstave/mistergift/api/configuration/ApiWebConfiguration.java
+++ mistergift-api/src/main/java/com/gvstave/mistergift/api/configuration/ApiWebConfiguration.java
@@ -1,5 +1,7 @@
 package com.gvstave.mistergift.api.configuration;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.gvstave.mistergift.api.Api;
 import com.gvstave.mistergift.data.configuration.DataWebConfiguration;
 import com.gvstave.mistergift.provider.Providers;
@@ -8,7 +10,12 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 /**
  * The Spring context initializer.
@@ -28,5 +35,20 @@ public class ApiWebConfiguration {
         return new SwaggerConfig();
     }
 
-
+    /**
+     * Jackson builder.
+     *
+     * @return the jackson 2 object mapper builder
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilder jacksonBuilder() {
+        final Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+        builder.serializationInclusion(JsonInclude.Include.NON_EMPTY);
+        builder.propertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE);
+        final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
+        builder.indentOutput(true).dateFormat(dateFormat);
+        return builder;
+    }
 }
