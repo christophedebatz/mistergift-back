cc67216cbae0774cd0a5085fbfe256b1f27ae5b9
Christophe de Batz <christophe.db@gmail.com>
2017-04-13 19:34:38 +0200

    doc(gift-dto): add comment to dto

--- mistergift-data/src/main/java/com/gvstave/mistergift/data/service/dto/GiftDto.java
+++ mistergift-data/src/main/java/com/gvstave/mistergift/data/service/dto/GiftDto.java
@@ -8,41 +8,82 @@ import com.gvstave.mistergift.data.domain.jpa.User;
 
 import java.io.Serializable;
 
+/**
+ * The gift dto.
+ */
 @JsonAutoDetect
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class GiftDto implements Serializable {
 
+    /** The serial version UUID. */
     private static final long serialVersionUID = 1L;
 
+    /** The event. */
     private Event event;
 
+    /** The user. */
     private User user;
 
+    /** The product id. */
     private String productId;
 
+    /**
+     * Constructor.
+     */
     public GiftDto() {
+        // this is for jackson
     }
 
+    /**
+     * Returns the event.
+     *
+     * @return The event.
+     */
     public Event getEvent() {
         return event;
     }
 
+    /**
+     * Sets the event.
+     *
+     * @param event The event.
+     */
     public void setEvent(Event event) {
         this.event = event;
     }
 
+    /**
+     * Returns the user.
+     *
+     * @return The user.
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Sets the user.
+     *
+     * @param user The user.
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * Returns the product id.
+     *
+     * @return The product id.
+     */
     public String getProductId() {
         return productId;
     }
 
+    /**
+     * Sets the product id.
+     *
+     * @param productId The product id.
+     */
     public void setProductId(String productId) {
         this.productId = productId;
     }
